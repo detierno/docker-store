b53a41833343cd81d3961ef21b0e91c2d7d17ddc
Denis Tierno <de.tierno@gmail.com>
2014-11-12 10:43:01 -0200

    changed db adapter and added style

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -6,6 +6,7 @@
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
 Spree.config do |config|
+  config.logo = "logo.png"
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
