11c19200dff4bad3cf41f9c4fd3c7925044eb063
Denis Tierno <de.tierno@gmail.com>
2014-11-12 10:56:11 -0200

    back with spree and add unicorn

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,5 +1,13 @@
-#Spree.config do |config|
-  #config.logo = "logo.png"
-#end
+# Configure Spree Preferences
+#
+# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
+#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
+#
+# In order to initialize a setting do:
+# config.setting_name = 'new value'
+Spree.config do |config|
+  config.logo = "logo.png"
+  # config.track_inventory_levels = false
+end
 
-#Spree.user_class = "Spree::User"
+Spree.user_class = "Spree::User"
