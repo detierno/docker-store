3484cdf6ad05f5904d1398aff88976d007fa3316
Denis Tierno <de.tierno@gmail.com>
2014-11-12 12:09:44 -0200

    set correct url path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    :path => '/spree/products/:id/:style/:basename.:extension',
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY'],
