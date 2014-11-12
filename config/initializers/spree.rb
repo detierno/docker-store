36316a7886ac04219799b1dbe2d7ad1a3b234c00
Denis Tierno <de.tierno@gmail.com>
2014-11-12 10:52:07 -0200

    removed spree temporarely

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,15 +1,5 @@
-# Configure Spree Preferences
-#
-# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
-#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
-#
-# In order to initialize a setting do:
-# config.setting_name = 'new value'
-Spree.config do |config|
-  config.logo = "logo.png"
-  # Example:
-  # Uncomment to stop tracking inventory levels in the application
-  # config.track_inventory_levels = false
-end
+#Spree.config do |config|
+  #config.logo = "logo.png"
+#end
 
-Spree.user_class = "Spree::User"
+#Spree.user_class = "Spree::User"
