3efba3da5f47d32628ea96d25f90c376234aa064
Denis Tierno <de.tierno@gmail.com>
2015-02-24 12:16:20 -0300

    add aws config

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,10 +1,33 @@
-# Configure Spree Preferences
-#
-# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
-#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
-#
-# In order to initialize a setting do:
-# config.setting_name = 'new value'
+attachment_config = {
+
+  s3_credentials: {
+    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    bucket:            ENV['S3_BUCKET_NAME']
+  },
+
+  storage:        :s3,
+  s3_headers:     { "Cache-Control" => "max-age=31557600" },
+  s3_protocol:    "https",
+  bucket:         ENV['S3_BUCKET_NAME'],
+  url:            ":s3_domain_url",
+
+  styles: {
+      mini:     "48x48>",
+      small:    "100x100>",
+      product:  "240x240>",
+      large:    "600x600>"
+  },
+
+  path:           "/spree/:class/:id/:style/:basename.:extension",
+  default_url:    "/spree/:class/:id/:style/:basename.:extension",
+  default_style:  "product"
+}
+
+attachment_config.each do |key, value|
+  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+end
+
 Spree.config do |config|
   config.logo = "logo.png"
   config.admin_interface_logo = "logo.png"
